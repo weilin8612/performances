337825aec28800af134a253b64d3e8116e98dc86
weilin <weilin@localhost>
2019-08-06 01:51:05 +0800

    separated 2 main logics

--- PrintStatement.py
+++ PrintStatement.py
@@ -4,10 +4,23 @@ import json
 
 def statment(invoice, plays):
 
-    result = 'Statement for {}'.format(invoice['customer'])
+    return rederPlainText(createStatementData(invoice, plays))
 
-    for perf in invoice['performances']:
-        result += '{}: {:.2f}  {} seats'.format(playFor(perf)['name'], anountFor(perf) / 100, perf['audience'])
+
+
+def createStatementData(invoice, plays):
+    result = {}
+    result['customer'] = invoice['customer']
+    result['performances'] = invoice['performances']
+    result['plays'] = plays
+    return result
+
+def rederPlainText(statementData):
+    result = 'Statement for {}'.format(statementData['customer'])
+
+    for perf in statementData['performances']:
+        result += '{}: {:.2f}  {} seats'.format(playFor(perf, statementData['plays'])['name'], anountFor(perf, statementData['plays']) / 100,
+                                                perf['audience'])
 
     totalAmout = appleSauce()
     volumeCreadits = totalVolumeCreadits()
@@ -17,37 +30,36 @@ def statment(invoice, plays):
 
     return result
 
-
 def appleSauce():
     result =0
     for perf in invoice['performances']:
-        result += anountFor(perf)
+        result += anountFor(perf, plays)
     return result
 
 def totalVolumeCreadits():
     result =0
     for perf in invoice['performances']:
-        result += volumeCreaditsFor(perf)
+        result += volumeCreaditsFor(perf, plays)
     return result
 
 
-def volumeCreaditsFor(perf):
+def volumeCreaditsFor(perf, plays):
     result = 0
     result += max(perf['audience'] - 30, 0)
-    if playFor(perf)['type'] == 'comedy':
+    if playFor(perf, plays)['type'] == 'comedy':
         result += floor(perf['audience'] / 5)
     return result
 
 
 
-def anountFor(aperformance_dict):
+def anountFor(aperformance_dict, plays):
     result = 0
-    if playFor(aperformance_dict)['type'] == 'tragedy':
+    if playFor(aperformance_dict, plays)['type'] == 'tragedy':
         result = 40000
         if aperformance_dict['audience'] > 30:
             result += 1000 * (aperformance_dict['audience'] - 30)
 
-    elif playFor(aperformance_dict)['type'] == 'comedy':
+    elif playFor(aperformance_dict, plays)['type'] == 'comedy':
         result = 30000
         if aperformance_dict['audience'] > 20:
             result += 10000 + 500 * (aperformance_dict['audience'] - 20)
@@ -57,7 +69,7 @@ def anountFor(aperformance_dict):
         print('PerformancesTypeError')
     return result
 
-def playFor(aperformance_dict):
+def playFor(aperformance_dict, plays):
     return plays[aperformance_dict['playID']]
 
 
